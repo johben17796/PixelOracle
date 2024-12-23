ce59de618b82d815e53548a527042ad7bca55cdc
Christopher Makousky <christopher.makousky@gmail.com>
2024-12-23 17:18:46 -0600

    Changed the console message of the addFavorites function to 'Updating Favorites List.'

--- client/src/api/favoriteGames-api.tsx
+++ client/src/api/favoriteGames-api.tsx
@@ -20,7 +20,7 @@ const getFavorites = async (user_id: number) => {
 
 const addFavorites = async (user_id: number , newFavorites: RawgData[]) => {
     // console.log({"favorites": newFavorites});
-    console.log(`Adding newFavorites to user favorite list.`);
+    console.log(`Updating Favorites List.`);
 try {
     const request = await fetch(`/api/users/addFavoriteGames/${user_id}`, {
     headers: {
