c04ac44eef39e3c0f322ee8e8f1409ab57c967a4
lucashallman <lucas.hallmanc@gmail.com>
2024-12-23 01:55:52 -0600

    CSS Overhaul pt1

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,13 @@
 //imports
 
-import NavBar from "./Nav";
+// import NavBar from "./Nav";
 
 //display function
 export default function Header() {
     return (
         <>
         <img className="logo" src="/img/PixelOracle.png" alt="PixelOracle"></img>
-        <NavBar />
+        {/* <NavBar /> */}
         <button id="loginButton" type="submit">LOGIN</button>
         </>
     )
